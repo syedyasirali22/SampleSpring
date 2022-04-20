9979675e08a802ebf904b5d2dc4572e83c941c78
syedyasirali22 <syed.yasirali2@gmail.com>
2022-04-20 10:48:28 +0530

    init commit

--- src/main/java/com/example/first/WelcomeController.java
+++ src/main/java/com/example/first/WelcomeController.java
@@ -4,16 +4,12 @@ import com.dumpclasses.Employee;
 import com.dumpclasses.EmployeeDatabase;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.swing.text.html.Option;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 public class WelcomeController {
-
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -22,8 +18,19 @@ public class WelcomeController {
 
     @GetMapping("/reduce")
     public String reduce() {
+        List<Integer> integers = Arrays.asList(10,20,50);
+        Integer sum = integers.stream()
+                .reduce((a, b) -> a+b)
+                .get();
+
+        System.out.println("Sum using reduce method " + sum);
+
+
         List<String> strings = Arrays.asList("Core Java", "HIbernate");
-        String longestString  = strings.stream().reduce((str1, str2) -> str1.length() > str2.length() ? str1 : str2).get();
+        String longestString  = strings
+                .stream()
+                .reduce((str1, str2) -> str1.length() > str2.length() ? str1 : str2)
+                .get();
         System.out.println("Longest String : " + longestString);
 
         Double averageSalary = EmployeeDatabase.employeeList.stream()
@@ -42,7 +49,9 @@ public class WelcomeController {
         Employee employee = new Employee(null, "A", "Manager");
         Optional<Object> nullSalaryOptional = Optional.ofNullable(employee.getSalary());
         Optional<String> designationOptional = Optional.ofNullable(employee.getDesignation());
-        System.out.println(designationOptional.map(String::toUpperCase).orElseGet(()->"Some default designation"));
+        System.out.println(designationOptional
+                .map(String::toUpperCase)
+                .orElseGet(()->"Some default designation"));
 
 
         return "Welcome to Spring Boot";
